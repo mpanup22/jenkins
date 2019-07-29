be9a6efb95da50fd0451a704d56c983e803d9166
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-07-29 18:25:20 +0530

    Update module.ec2.tf

--- layers/DEV/module.ec2.tf
+++ layers/DEV/module.ec2.tf
@@ -2,6 +2,8 @@ module "ec2" {
   source = "../../modules/ec2"
   
   region    = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
   availability_zone = "${var.availability_zone}"
   ami= "${var.ami}"
   instance_type="${var.instance_type}"
