ebccbe383fd59818fc2ac81e1dfcee120ce32d57
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-07-30 17:41:54 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "firstmachine"{
     ami= "${var.ami}"
     instance_type="${var.instance_type}"
     availability_zone = "${var.availability_zone}"
-    key_name= "${var.key_name}"
+    ## key_name= "${var.key_name}"
     security_groups = [ "${aws_security_group.SG.name}"]
 }
 resource "aws_security_group" "SG" {
