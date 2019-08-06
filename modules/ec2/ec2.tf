8f1fa429532cf470697122c312cd7d5e1f67b97e
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-08-06 12:41:03 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -22,12 +22,22 @@ resource "aws_security_group" "SG" {
    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-//resource "aws_volume_attachment" "ebs_att" {
-//device_name = "/dev/sdh"
-//volume_id = "${aws_ebs_volume.pract.id}"
-//instance_id = "${aws_instance.firstmachine.id}"
- //}
-//data "aws_kms_key" "kmskey" {
- // key_id = "alias/DEV_KMS_KEY"
-  
-//}
+resource "aws_volume_attachment" "ebs_att" {
+device_name = "/dev/sdh"
+volume_id = "${aws_ebs_volume.pract.id}"
+instance_id = "${aws_instance.firstmachine.id}"
+ }
+data "aws_kms_key" "kmskey" {
+  key_id = "alias/DEV_KMS_KEY"
+  }
+
+resource "aws_ebs_volume" "pract" {
+  availability_zone = "us-east-2a"
+  size              = 20
+
+  tags = {
+    Name = "ebs"
+  }
+  encrypted       = true
+  kms_key_id = "${data.aws_kms_key.kmskey.arn}"
+}
