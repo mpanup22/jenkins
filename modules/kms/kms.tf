5bf0d2297fef71608f84ae450f49a5de08f42369
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-08-06 12:33:29 +0530

    Update kms.tf

--- modules/kms/kms.tf
+++ modules/kms/kms.tf
@@ -4,7 +4,7 @@ data "aws_kms_key" "kmskey" {
 
 resource "aws_s3_bucket_object" "s3bucket" {
   kms_key_id = "${data.aws_kms_key.kmskey.arn}"
-  bucket = "devpract"
+  bucket = "devpract22"
   key = "outputs.txt"
 
 }
