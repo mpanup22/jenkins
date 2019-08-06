9b6c97b31709cebdf263fc1a16a1536bfd4fbb13
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-08-06 12:04:29 +0530

    Update ebs.tf

--- modules/ebs/ebs.tf
+++ modules/ebs/ebs.tf
@@ -1,8 +1,8 @@
-//resource "aws_volume_attachment" "ebs_att" {
-//device_name = "/dev/sdh"
-//volume_id = "${aws_ebs_volume.pract.id}"
-//instance_id = "${var.ami}"
-//}
+resource "aws_volume_attachment" "ebs_att" {
+device_name = "/dev/sdh"
+volume_id = "${aws_ebs_volume.pract.id}"
+instance_id = "${aws_instance.firstmachine.id}"
+ }
 data "aws_kms_key" "kmskey" {
     key_id = "alias/DEV_KMS_KEY"
     }
